9b32656c55833797b58d8c3c5601c24bc6d2cfb0
thepydtyper <srjordan82@gmail.com>
2020-01-26 18:22:21 -0500

    fixed layout

--- mygui.py
+++ mygui.py
@@ -17,29 +17,30 @@ def get_stocks():
 
 
 root = Tk()
-
-
-output_label = Label(root, text="Output: ")
-output_label.grid(row=0, column=0)
-
-output_window = Label(root, width=30, bg="black", fg="green")
-output_window.grid(row=0, column=1)
-
-button_weather = Button(root, text="Weather", command=get_weather)
-button_weather.grid(row=1, column=0)
-
-button_news = Button(root, text="News", command=get_news)
-button_news.grid(row=1, column=1)
-
-button_stocks = Button(root, text="Stocks", command=get_stocks)
-button_stocks.grid(row=1, column=2)
+root.title("Deskpy")
 
 input_label = Label(root, text="Input: ")
-input_label.grid(row=2, column=0)
+input_label.grid(row=0, column=0)
 
-input_window = Entry(root, width=20, bg="black", fg="green", borderwidth=5)
-input_window.grid(row=2, column=1)
+input_window = Entry(root, width=50, bg="black", fg="green", borderwidth=5)
+input_window.grid(row=0, column=1, columnspan=3)
 
+button_weather = Button(root, text="Weather",
+                        command=get_weather, padx=30, pady=20)
+button_weather.grid(row=1, column=1)
+
+button_news = Button(root, text="News", command=get_news, padx=30, pady=20)
+button_news.grid(row=1, column=2)
+
+button_stocks = Button(root, text="Stocks",
+                       command=get_stocks, padx=30, pady=20)
+button_stocks.grid(row=1, column=3)
+
+output_label = Label(root, text="Output: ")
+output_label.grid(row=2, column=0)
+
+output_window = Label(root, width=44, bg="black", fg="green")
+output_window.grid(row=2, column=1, columnspan=3)
 
 
 root.mainloop()
